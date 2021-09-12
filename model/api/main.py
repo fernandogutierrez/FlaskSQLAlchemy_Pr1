9da611dfcbf9cea68f98ccf9e1fdeab8c97b0bc2
Fernando.Gutierrez <fernando.gutierrez@jalasoft.com>
2021-09-12 15:59:56 -0400

    Initial commit

---
+++ model/api/main.py
@@ -0,0 +1,79 @@
+from flask import request, jsonify, abort
+from db_connector import DBConnector
+from content_manager import ContentManager
+
+app = DBConnector().app
+API_NAME = "/api/v1"
+
+
+@app.route(f"{API_NAME}/pets", methods=["GET"])
+def get_pets():
+    """
+    get_pets --> Get the list of all pets available
+    Return:
+        pets(dict): list of pets.
+    """
+    return jsonify(ContentManager.get_pets())
+
+
+@app.route(f"{API_NAME}/pet", methods=["POST"])
+def post_pet():
+    """
+    save_pet --> Save a given pet into a database.
+    Return:
+        pet(dict): The given pet that was saved.
+    """
+    result = ContentManager.save_pet(request.json)
+    if result == {}:
+        abort(400, "Invalid request. Check the data sent")
+    return result
+
+
+@app.route(f"{API_NAME}/pet/<pet_id>", methods=["GET"])
+def get_pet_by_id(pet_id):
+    """
+    get_pet_by_id --> Get a pet given an id
+    Args:
+        pet_id(int): The pet id to look for
+    Return:
+        pet(dict): A pet
+    """
+    return jsonify(ContentManager.get_one_pet(pet_id))
+
+
+@app.route(f"{API_NAME}/pets/filter", methods=["GET"])
+def get_pets_with_args():
+    """
+    get_pet_with_args --> Get a collections of pets given a name, type or gender and type
+    e.g: http://localhost:5000/api/v1/pets/filter?type=Cat
+    Return:
+        pets(dict): list of pets.
+    """
+    return jsonify(ContentManager.get_pets_with_args(request.args))
+
+
+@app.route(f"{API_NAME}/pets", methods=["POST"])
+def get_pets_with_filters():
+    """
+    get_pets_with_filters --> Get a collections of pets given criteria in the json request
+    Return:
+        pets(dict): list of pets.
+    """
+    return jsonify(ContentManager.get_one_using_filters(request.json))
+
+
+@app.route(f"{API_NAME}/pet/<pet_id>", methods=["DELETE"])
+def delete_pet(pet_id):
+    """
+    delete_pet --> Delete a pet given an id.
+    Args:
+        pet_id(integer): The pet id to delete.
+    Return:
+        result(dict): A dictionary that indicates if the pet was removed
+    """
+    if ContentManager.delete_pet(pet_id) != 1:
+        return jsonify(Message=f"The given pet id <{pet_id}> was not found"), 404
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True, port=5000, use_reloader=True)
---
+++ model/content_manager.py
@@ -0,0 +1,52 @@
+from db_connector import DBConnector
+from pet import Pet, PetSchema
+from model.utils import datetime
+
+db = DBConnector().db
+
+
+class ContentManager:
+    @staticmethod
+    def get_pets():
+        pets = Pet.query.all()
+        return PetSchema.dump_many(pets)
+
+    @staticmethod
+    def save_pet(pet):
+        if "name" and "type" and "gender" and "date" not in pet or not datetime.validate(pet["date"]):
+            return {}
+        pet["date"] = datetime.cast(pet.get("date"), format='%d/%m/%Y')
+        new_pet = Pet(**pet)
+        db.session.add(new_pet)
+        db.session.commit()
+        return PetSchema.dump_one(new_pet)
+
+    @staticmethod
+    def delete_pet(pet_id):
+        try:
+            obj = Pet.query.filter_by(id=pet_id).one()
+            return db.session.delete(obj)
+        except:
+            return {}
+
+    @staticmethod
+    def get_one_pet(pet_id):
+        pet_query_result = Pet.query.filter_by(id=pet_id).first()
+        return PetSchema.dump_one(pet_query_result)
+
+    @staticmethod
+    def get_one_using_filters(filters_criteria):
+        results = Pet.query.filter_by(**filters_criteria).all()
+        return PetSchema.dump_many(results)
+
+    @staticmethod
+    def get_pets_with_args(args):
+        query_results = []
+        if "name" in args:
+            query_results = Pet.query.filter_by(name=args.get('name')).order_by(Pet.name).all()
+        elif "type" and "gender" in args:
+            query_results = Pet.query.filter_by(type=args.get('type'), gender=args.get('gender')).order_by(
+                Pet.date).all()
+        elif "type" in args:
+            query_results = Pet.query.filter_by(type=args.get('type')).order_by(Pet.date).all()
+        return PetSchema.dump_many(query_results)
